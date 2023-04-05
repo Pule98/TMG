897f50361e132f5191e90d7a3da866365d3845f8
Pule-Taunyane <Pule.taunyane98@gmail.com>
2023-04-05 14:02:34 +0200

    Add project files.

---
+++ Maze game/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿
+namespace Maze_game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.exitGamebtn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.pictureBox10 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pointslbl = new System.Windows.Forms.Label();
+            this.pictureBox11 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // exitGamebtn
+            // 
+            this.exitGamebtn.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitGamebtn.Location = new System.Drawing.Point(2, 451);
+            this.exitGamebtn.Name = "exitGamebtn";
+            this.exitGamebtn.Size = new System.Drawing.Size(124, 50);
+            this.exitGamebtn.TabIndex = 0;
+            this.exitGamebtn.Text = "Exit Game";
+            this.exitGamebtn.UseVisualStyleBackColor = true;
+            this.exitGamebtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Chocolate;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.No;
+            this.pictureBox1.Location = new System.Drawing.Point(132, 41);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(422, 63);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseHover += new System.EventHandler(this.pictureBox1_MouseHover);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Chocolate;
+            this.pictureBox2.Cursor = System.Windows.Forms.Cursors.No;
+            this.pictureBox2.Location = new System.Drawing.Point(483, 98);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(71, 325);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.MouseHover += new System.EventHandler(this.pictureBox2_MouseHover);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Chocolate;
+            this.pictureBox3.Cursor = System.Windows.Forms.Cursors.No;
+            this.pictureBox3.Location = new System.Drawing.Point(396, 98);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(52, 166);
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.MouseHover += new System.EventHandler(this.pictureBox3_MouseHover);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Chocolate;
+            this.pictureBox4.Cursor = System.Windows.Forms.Cursors.No;
+            this.pictureBox4.Location = new System.Drawing.Point(132, 132);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(234, 48);
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.MouseHover += new System.EventHandler(this.pictureBox4_MouseHover);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.Chocolate;
+            this.pictureBox5.Cursor = System.Windows.Forms.Cursors.No;
+            this.pictureBox5.Location = new System.Drawing.Point(132, 176);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(63, 278);
+            this.pictureBox5.TabIndex = 5;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.MouseHover += new System.EventHandler(this.pictureBox5_MouseHover);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.Color.Chocolate;
+            this.pictureBox6.Cursor = System.Windows.Forms.Cursors.No;
+            this.pictureBox6.Location = new System.Drawing.Point(236, 214);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(167, 50);
+            this.pictureBox6.TabIndex = 6;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.MouseHover += new System.EventHandler(this.pictureBox6_MouseHover);
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.BackColor = System.Drawing.Color.Chocolate;
+            this.pictureBox7.Cursor = System.Windows.Forms.Cursors.No;
+            this.pictureBox7.Location = new System.Drawing.Point(236, 291);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(63, 163);
+            this.pictureBox7.TabIndex = 7;
+            this.pictureBox7.TabStop = false;
+            this.pictureBox7.MouseHover += new System.EventHandler(this.pictureBox7_MouseHover);
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.BackColor = System.Drawing.Color.Chocolate;
+            this.pictureBox8.Cursor = System.Windows.Forms.Cursors.No;
+            this.pictureBox8.Location = new System.Drawing.Point(132, 451);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(422, 52);
+            this.pictureBox8.TabIndex = 8;
+            this.pictureBox8.TabStop = false;
+            this.pictureBox8.MouseHover += new System.EventHandler(this.pictureBox8_MouseHover);
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.BackColor = System.Drawing.Color.Chocolate;
+            this.pictureBox9.Cursor = System.Windows.Forms.Cursors.No;
+            this.pictureBox9.Location = new System.Drawing.Point(288, 291);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(160, 99);
+            this.pictureBox9.TabIndex = 9;
+            this.pictureBox9.TabStop = false;
+            this.pictureBox9.MouseHover += new System.EventHandler(this.pictureBox9_MouseHover);
+            // 
+            // pictureBox10
+            // 
+            this.pictureBox10.BackColor = System.Drawing.Color.Chocolate;
+            this.pictureBox10.Cursor = System.Windows.Forms.Cursors.No;
+            this.pictureBox10.Location = new System.Drawing.Point(396, 326);
+            this.pictureBox10.Name = "pictureBox10";
+            this.pictureBox10.Size = new System.Drawing.Size(52, 99);
+            this.pictureBox10.TabIndex = 10;
+            this.pictureBox10.TabStop = false;
+            this.pictureBox10.MouseHover += new System.EventHandler(this.pictureBox10_MouseHover);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(255, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 29);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Level 1";
+            // 
+            // pointslbl
+            // 
+            this.pointslbl.AutoSize = true;
+            this.pointslbl.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pointslbl.Location = new System.Drawing.Point(13, 151);
+            this.pointslbl.Name = "pointslbl";
+            this.pointslbl.Size = new System.Drawing.Size(26, 29);
+            this.pointslbl.TabIndex = 12;
+            this.pointslbl.Text = "0";
+            // 
+            // pictureBox11
+            // 
+            this.pictureBox11.BackColor = System.Drawing.Color.White;
+            this.pictureBox11.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox11.Location = new System.Drawing.Point(516, 429);
+            this.pictureBox11.Name = "pictureBox11";
+            this.pictureBox11.Size = new System.Drawing.Size(38, 22);
+            this.pictureBox11.TabIndex = 13;
+            this.pictureBox11.TabStop = false;
+            this.pictureBox11.MouseEnter += new System.EventHandler(this.pictureBox11_MouseEnter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(551, 502);
+            this.Controls.Add(this.pictureBox11);
+            this.Controls.Add(this.pointslbl);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox10);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.exitGamebtn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "TMG";
+            this.Load += new System.EventHandler(this.exitbtn_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button exitGamebtn;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.PictureBox pictureBox10;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label pointslbl;
+        private System.Windows.Forms.PictureBox pictureBox11;
+    }
+}
+
---
+++ Maze game/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Maze_game
+{
+    public partial class Form1 : Form
+    {
+        public static int points = 3;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void exitbtn_Load(object sender, EventArgs e)
+        {
+            pointslbl.Text ="Points: " + points.ToString();
+        }
+
+        private void pictureBox1_MouseHover(object sender, EventArgs e)
+        {
+            points--;
+
+            pointslbl.Text = "Points: " + points.ToString();
+
+            if (points == 0)
+            {
+                MessageBox.Show("Game Over!");
+                Application.Exit();
+            }
+        }
+
+        private void pictureBox4_MouseHover(object sender, EventArgs e)
+        {
+            points--;
+
+            pointslbl.Text = "Points: " + points.ToString();
+
+            if (points == 0)
+            {
+                MessageBox.Show("Game Over!");
+                Application.Exit();
+            }
+        }
+
+        private void pictureBox3_MouseHover(object sender, EventArgs e)
+        {
+            points--;
+
+            pointslbl.Text = "Points: " + points.ToString();
+
+            if (points == 0)
+            {
+                MessageBox.Show("Game Over!");
+                Application.Exit();
+            }
+        }
+
+        private void pictureBox6_MouseHover(object sender, EventArgs e)
+        {
+            points--;
+
+            pointslbl.Text = "Points: " + points.ToString();
+
+            if (points == 0)
+            {
+                MessageBox.Show("Game Over!");
+                Application.Exit();
+            }
+        }
+
+        private void pictureBox5_MouseHover(object sender, EventArgs e)
+        {
+            points--;
+
+            pointslbl.Text = "Points: " + points.ToString();
+
+            if (points == 0)
+            {
+                MessageBox.Show("Game Over!");
+                Application.Exit();
+            }
+        }
+
+        private void pictureBox8_MouseHover(object sender, EventArgs e)
+        {
+            points--;
+
+            pointslbl.Text = "Points: " + points.ToString();
+
+            if (points == 0)
+            {
+                MessageBox.Show("Game Over!");
+                Application.Exit();
+            }
+        }
+
+        private void pictureBox7_MouseHover(object sender, EventArgs e)
+        {
+            points--;
+
+            pointslbl.Text = "Points: " + points.ToString();
+
+            if (points == 0)
+            {
+                MessageBox.Show("Game Over!");
+                Application.Exit();
+            }
+        }
+
+        private void pictureBox9_MouseHover(object sender, EventArgs e)
+        {
+            points--;
+
+            pointslbl.Text = "Points: " + points.ToString();
+
+            if (points == 0)
+            {
+                MessageBox.Show("Game Over!");
+                Application.Exit();
+            }
+        }
+
+        private void pictureBox10_MouseHover(object sender, EventArgs e)
+        {
+            points--;
+
+            pointslbl.Text = "Points: " + points.ToString();
+
+            if (points == 0)
+            {
+                MessageBox.Show("Game Over!");
+                Application.Exit();
+            }
+        }
+
+        private void pictureBox2_MouseHover(object sender, EventArgs e)
+        {
+            points--;
+
+            pointslbl.Text = "Points: " + points.ToString();
+
+            if (points == 0)
+            {
+                MessageBox.Show("Game Over!");
+                Application.Exit();
+            }
+        }
+
+        private void pictureBox11_MouseEnter(object sender, EventArgs e)
+        {
+            MessageBox.Show("Congratulations!");
+        }
+    }
+}
